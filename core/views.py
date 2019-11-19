98372e71edefbe44bd5fcf8433e3b8c5543c5ad7
Emir Amanbekov <amanbekoff@gmail.com>
2019-11-19 20:04:17 +0600

    Update FlightOptimizerView
    * Install ufo

--- core/views.py
+++ core/views.py
@@ -1,7 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from ufo.optimizers import DPKFlightOptimizer
+from ufo.clients import KiwiClient
 
 
 class FlightOptimizerView(APIView):
     def get(self, request):
-        return Response()
+        departure = request.query_params.get('from')
+        destinations = request.query_params.getlist('to')
+        optimizer = DPKFlightOptimizer(departure, destinations, KiwiClient)
+        flight = optimizer.process()
+        return Response(
+            {
+                'destination': flight.destination.city.name,
+                'dpk': flight.dpk,
+            }
+        )
